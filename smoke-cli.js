165329932681f2d7902f36658f0c5c2972a7b995
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2020-08-27 13:06:43 +0530

    Added CORS capabilities

--- smoke-cli.js
+++ smoke-cli.js
@@ -5,29 +5,30 @@ const {createServer, startServer} = require('./lib/smoke');
 const help = `Usage: smoke [<mocks_folder>] [options]
 
 Base options:
-  -p, --port <num>        Server port           [default: 3000]
-  -h, --host <host>       Server host           [default: "localhost"]
-  -s, --set <name>        Mocks set to use      [default: none]
-  -n, --not-found <glob>  Mocks for 404 errors  [default: "404.*"]
-  -i, --ignore <glob>     Files to ignore       [default: none]
-  -k, --hooks <file>      Middleware hooks      [default: none]
-  -x, --proxy <host>      Fallback proxy if no mock found
-  -l, --logs              Enable server logs
-  -v, --version           Show version
-  --help                  Show help
+  -p, --port <num>                  Server port           [default: 3000]
+  -h, --host <host>                 Server host           [default: "localhost"]
+  -s, --set <name>                  Mocks set to use      [default: none]
+  -n, --not-found <glob>            Mocks for 404 errors  [default: "404.*"]
+  -i, --ignore <glob>               Files to ignore       [default: none]
+  -k, --hooks <file>                Middleware hooks      [default: none]
+  -x, --proxy <host>                Fallback proxy if no mock found
+  -o, --allow-cors [all|<hosts>]    Enable CORS requests  [default: none]
+  -l, --logs                        Enable server logs
+  -v, --version                     Show version
+  --help                            Show help
 
 Mock recording:
-  -r, --record <host>     Proxy & record requests if no mock found
-  -c, --collection <file> Save to single file mock collection
-  -d, --depth <N>         Folder depth for mocks  [default: 1]
-  -a, --save-headers      Save response headers
-  -q, --save-query        Save query parameters
+  -r, --record <host>               Proxy & record requests if no mock found
+  -c, --collection <file>           Save to single file mock collection
+  -d, --depth <N>                   Folder depth for mocks  [default: 1]
+  -a, --save-headers                Save response headers
+  -q, --save-query                  Save query parameters
 `;
 
 function run(args) {
   const options = minimist(args, {
     number: ['port', 'depth'],
-    string: ['host', 'set', 'not-found', 'record', 'ignore', 'hooks', 'proxy', 'collection'],
+    string: ['host', 'set', 'not-found', 'record', 'ignore', 'hooks', 'proxy', 'collection', 'allow-cors'],
     boolean: ['help', 'version', 'logs', 'save-headers', 'save-query'],
     alias: {
       p: 'port',
@@ -42,7 +43,8 @@ function run(args) {
       q: 'save-query',
       i: 'ignore',
       k: 'hooks',
-      x: 'proxy'
+      x: 'proxy',
+      o: 'allow-cors'
     }
   });
 
@@ -69,7 +71,8 @@ function run(args) {
     collection: options.collection,
     depth: options.depth,
     saveHeaders: options['save-headers'],
-    saveQueryParams: options['save-query']
+    saveQueryParams: options['save-query'],
+    cors: options['allow-cors']
   });
 
   if (app) {
